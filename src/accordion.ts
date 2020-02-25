70a322af0fd849344a70845389fb84f2f7c18944
Jared Forth <jaredforthdev@gmail.com>
2020-02-25 10:27:34 -0600

    enable optional stylesheet

--- src/accordion.ts
+++ src/accordion.ts
@@ -11,6 +11,7 @@ export class Accordion {
                 this.toggle_open();
             }
         }
+        this.set_styles();
     }
     toggle_open() {
         if (this.triggers && this.content) {
@@ -23,6 +24,18 @@ export class Accordion {
                         content.classList.add('open');
                     }
                 }
+            }
+        }
+    }
+    set_styles() {
+        let style: HTMLStyleElement = document.createElement('style');
+        if (style) {
+            document.head.appendChild(style);
+            let sheet: CSSStyleSheet | null = style.sheet as CSSStyleSheet;
+            if (sheet) {
+                sheet.insertRule('.accordion { list-style: none }');
+                sheet.insertRule('.accordion li p { display: none }');
+                sheet.insertRule('.accordion li p.open { display: block }');
             }
         }
     }
