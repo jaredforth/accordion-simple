dbd52dfd62a20955ea0b609c10cb8779359b6297
Jared Forth <jaredforthdev@gmail.com>
2021-03-23 09:44:20 -0500

    fix some edgecase bugs

--- src/accordion.ts
+++ src/accordion.ts
@@ -2,21 +2,27 @@ export class Accordion {
     containers: HTMLCollection;
     listItems: HTMLCollectionOf<HTMLLIElement> | undefined;
     triggers: HTMLCollectionOf<HTMLAnchorElement> | undefined;
+    triggersArray: Array<HTMLAnchorElement> | undefined;
+    listItemsArray: Array<HTMLLIElement> | undefined;
     constructor() {
         this.containers = document.getElementsByClassName('accordion');
         let containers_array = Array.from(this.containers);
         containers_array.map(c => {
             this.listItems = c.getElementsByTagName('li');
             this.triggers = c.getElementsByTagName('a');
+            this.filter_triggers();
+            this.filter_list_items();
             this.toggle_open();
         });
         this.set_styles();
     }
     toggle_open() {
-        if (this.triggers && this.listItems) {
-            for (let i = 0; i < this.triggers.length; i++) {
-                let li = this.listItems[i];
-                this.triggers[i].addEventListener('click', function () {
+        console.log(this.triggersArray);
+        console.log(this.listItemsArray);
+        if (this.triggersArray && this.listItemsArray) {
+            for (let i = 0; i < this.triggersArray.length; i++) {
+                let li = this.listItemsArray[i];
+                this.triggersArray[i].addEventListener('click', function () {
                     if (li.classList.contains('open')) {
                         li.classList.remove('open');
                     } else {
@@ -24,6 +30,28 @@ export class Accordion {
                     }
                 })
             }
+        }
+    }
+    filter_triggers() {
+        if (this.triggers) {
+            this.triggersArray =  Array.from(this.triggers).filter(t => {
+                if (t.parentElement) {
+                    if (t.parentElement.nodeName  !== 'DIV') {
+                        return t
+                    }
+                }
+            })
+        }
+    }
+    filter_list_items() {
+        if (this.listItems) {
+            this.listItemsArray =  Array.from(this.listItems).filter(t => {
+                if (t.parentElement) {
+                    if (t.parentElement.classList.contains('accordion')) {
+                        return t
+                    }
+                }
+            })
         }
     }
     set_styles() {
@@ -34,7 +62,7 @@ export class Accordion {
             if (sheet) {
                 sheet.insertRule('ul.accordion { list-style: none }');
                 sheet.insertRule('ul.accordion li div { display: none }');
-                sheet.insertRule('ul.accordion li.open div { display: block }');
+                sheet.insertRule('ul.accordion li.open div { display: block !important }');
             }
         }
     }
